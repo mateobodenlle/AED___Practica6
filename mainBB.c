5c52eb3efb8965881975d8bcab851779e9ddf71c
mateobodenlle <mateo.bodenlle@rai.usc.es>
2023-12-19 13:32:54 +0100

    Finalizado, con contadores

--- mainBB.c
+++ mainBB.c
@@ -16,6 +16,16 @@
 //Variables globales (contadores)
 int nNodos = 0, nPasosCriterio = 0, nPasosGenerar = 0, nPasosSolucion = 0, nPasosHermanos = 0, nPasosRetroceder = 0;
 
+//Estructura para el nodo
+typedef struct {
+    int tupla [N];
+    int nivel;
+    int bact;
+    int usadas[N];
+    float CI, BE, CS;
+}TIPOELEMENTO;
+typedef TIPOELEMENTO NODO;
+
 //Definición de matriz
 typedef int matriz[N][N];
 /*
@@ -32,13 +42,33 @@ matriz B = {{11,17,8,16,20,14},
             {10,14,12,11,15,13},
             {12,20,19,13,22,17}};
  
+//Implementación de funciones de BB
+int CI_trivial(){
 
+}
+int CS_trivial(){
+
+}
+
+int CI_precisa(){
+
+}
+int CS_precisa(){
+
+}
+
+void asignacionTrivial(){
+    //INICIALIZACIÓN
+    TLISTA
+}
+
+void asignacionPrecisa(){
+
+}
 //Implementación de funciones de Backtracking
-int criterio(int nivel, int s[N]) {
+int criterio(int nivel, int s[N], int usada[N]) {
     nPasosCriterio++;
-    for (int i = 0; i < nivel; i++)
-        if (s[nivel] == s[i]) return 0;
-    return 1;
+    return usada[s[nivel]]==1;
 }
 
 int masHermanos(int nivel, int s[N]) {
@@ -46,16 +76,20 @@ int masHermanos(int nivel, int s[N]) {
     return s[nivel]<(N-1);
 }
 
-int retroceder(int *nivel, int s[N],int* bact){
+int retroceder(int *nivel, int s[N],int* bact,int usada[N]){
     nPasosRetroceder++;
     (*bact)-=B[*nivel][s[*nivel]];
+    usada[s[*nivel]]--;
     s[*nivel]=valorDefecto;
     (*nivel)--;
 }
 
-int generar(int nivel, int s[N],int* bact) {
+int generar(int nivel, int s[N],int* bact, int usada[N]) {
+    if (s[nivel]!=-1)
+        usada[s[nivel]]--;
     nPasosGenerar++;
     s[nivel]++;
+    usada[s[nivel]]++;
     //nNodos++;
     if (s[nivel]==0)
         (*bact)+=B[nivel][s[nivel]];
@@ -63,9 +97,9 @@ int generar(int nivel, int s[N],int* bact) {
         (*bact)+=B[nivel][s[nivel]]-B[nivel][s[nivel]-1];
 }
 
-int solucion(int nivel, int s[N]){
+int solucion(int nivel, int s[N], int usada[N]){
     nPasosSolucion++;
-    return (nivel==(N-1) && criterio(nivel,s));
+    return (nivel==(N-1) && criterio(nivel,s,usada));
 }
 
 //Algoritmo de Backtracking
@@ -75,21 +109,22 @@ void backtracking(int s[N]){
     int voa = -999;
     int soa[N] = {valorDefecto,valorDefecto,valorDefecto};
     int bact = 0;
+    int usada[N] = {0,0,0};
 
     while(nivel!=-1){
-        generar(nivel,s,&bact);
-        if (solucion(nivel,s) && (bact>voa)){
+        generar(nivel,s,&bact,usada);
+        if (solucion(nivel,s,usada) && (bact>voa)){
             voa = bact;
             for (int i = 0; i<N; i++) soa[i] = s[i];
             //printf("\nSolucion encontrada! \t"); for (int i = 0; i<N; i++) printf("%d ",s[i]);
         }
-        if (criterio(nivel,s)) {
+        if (criterio(nivel,s,usada)) {
             if (nivel < (N - 1))
                 nivel++;
             nNodos++;
         }
         while (!masHermanos(nivel,s)&&(nivel>=0))
-            retroceder(&nivel,s,&bact);
+            retroceder(&nivel,s,&bact,usada);
 
     }
     for (int i = 0; i<N; i++) s[i] = soa[i];
